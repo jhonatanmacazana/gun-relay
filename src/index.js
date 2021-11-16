b40eb294ec8e489f68370765a0e23e0b569a64a2
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-11-16 12:42:25 -0500

    feat: relay peer with express

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const Gun = require("gun");
+
+const PORT = process.env.PORT || 8000;
+const TEMPORAL_DIR = "gundata";
+
+const startServer = () => {
+  const app = express();
+
+  app.use(Gun.serve);
+  app.use(express.static(__dirname));
+
+  const server = app.listen(PORT, () => {
+    console.log(`Listening at: http://localhost:${PORT}`);
+  });
+
+  Gun({ file: TEMPORAL_DIR, web: server });
+};
+
+startServer();
